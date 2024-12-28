5a2ff2d3604c5d93a006e49ad2147e3675daa0fa
Kim Goetzke <kbgoetzke@gmail.com>
2024-12-28 15:22:45 +0000

    Add NotFound page

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod pages;
 
+use crate::pages::*;
 use leptos::prelude::*;
 use leptos_router::components::*;
 use leptos_router::path;
-use crate::pages::*;
 
 fn main() {
   console_error_panic_hook::set_once();
@@ -23,11 +23,22 @@ fn App() -> impl IntoView {
         </div>
       </nav>
       <main>
-        <Routes fallback=|| "Not found.">
+        <Routes fallback=|| NotFound>
           <Route path=path!("/") view=Home />
           <Route path=path!("/blog") view=Blog />
         </Routes>
       </main>
     </Router>
+  }
+}
+
+#[component]
+fn NotFound() -> impl IntoView {
+  view! {
+    <div class="centered-layout">
+      <h1>404</h1>
+      <div class="py-6"></div>
+      <p>Sorry, something went wrong.</p>
+    </div>
   }
 }
