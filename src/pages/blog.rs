8144fd7e4fec0a4f96558d8e2f2ab0d4084b1c4a
Kim Goetzke <kbgoetzke@gmail.com>
2025-01-10 05:21:21 +0000

    Constrain blog width

--- src/pages/blog.rs
+++ src/pages/blog.rs
@@ -7,52 +7,54 @@ use leptos_router::components::*;
 #[component]
 pub(crate) fn Blog() -> impl IntoView {
   view! {
-    <div>
-      <h2>Blog</h2>
-      <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 3xl:grid-cols-5 4xl:grid-cols-6">
-        {move || {
-          let posts = crate::pages::blog_post::BLOG_POSTS.clone();
-          posts
-            .into_iter()
-            .map(|post| {
-              let date = Date::new(&JsValue::from_f64(post.created_at));
-              let formatted_date = format!("{}", date.to_locale_date_string("en-GB", &JsValue::undefined()));
-              view! {
-                <div class="flex flex-col rounded-lg transition bg-nord1 hover:drop-shadow-xl">
-                  <div class="flex overflow-hidden justify-center items-center max-h-32 rounded-t-lg">
-                    <A href=post
-                      .id
-                      .clone()>
-                      {move || {
-                        post
-                          .image
-                          .as_ref()
-                          .map(|img| view! { <img class="object-cover w-full h-full" src=img.clone() /> })
-                      }}
-                    </A>
-                  </div>
-                  <div class="p-4">
-                    <h3 class="group">
-                      <A href=post.id.clone()>
-                        <span class="underlined">{post.title.clone()}</span>
+    <div class="home-container">
+      <section class="content-section">
+        <h2>Blog</h2>
+        <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+          {move || {
+            let posts = crate::pages::blog_post::BLOG_POSTS.clone();
+            posts
+              .into_iter()
+              .map(|post| {
+                let date = Date::new(&JsValue::from_f64(post.created_at));
+                let formatted_date = format!("{}", date.to_locale_date_string("en-GB", &JsValue::undefined()));
+                view! {
+                  <div class="flex flex-col rounded-lg transition bg-nord1 hover:drop-shadow-xl">
+                    <div class="flex overflow-hidden justify-center items-center max-h-32 rounded-t-lg">
+                      <A href=post
+                        .id
+                        .clone()>
+                        {move || {
+                          post
+                            .image
+                            .as_ref()
+                            .map(|img| view! { <img class="object-cover w-full h-full" src=img.clone() /> })
+                        }}
                       </A>
-                    </h3>
+                    </div>
+                    <div class="p-4">
+                      <h3 class="group">
+                        <A href=post.id.clone()>
+                          <span class="underlined">{post.title.clone()}</span>
+                        </A>
+                      </h3>
+                    </div>
+                    <div class="overflow-hidden flex-grow p-4 max-h-48 text-ellipsis line-clamp-3">
+                      <p class="mb-4 text-nord6">{post.content.clone()}</p>
+                    </div>
+                    <div class="p-4 mt-auto">
+                      <p class="mb-2 text-xl text-nord3">
+                        {post.tags.iter().map(|tag| view! { <Tag tag=tag.clone() /> }).collect::<Vec<_>>()}
+                      </p>
+                      <p class="text-2xl font-m5 text-nord3">"Created: " {formatted_date}</p>
+                    </div>
                   </div>
-                  <div class="overflow-hidden flex-grow p-4 max-h-48 text-ellipsis line-clamp-3">
-                    <p class="mb-4 text-nord6">{post.content.clone()}</p>
-                  </div>
-                  <div class="p-4 mt-auto">
-                    <p class="mb-2 text-xl text-nord3">
-                      {post.tags.iter().map(|tag| view! { <Tag tag=tag.clone() /> }).collect::<Vec<_>>()}
-                    </p>
-                    <p class="text-2xl font-m5 text-nord3">"Created: " {formatted_date}</p>
-                  </div>
-                </div>
-              }
-            })
-            .collect::<Vec<_>>()
-        }}
-      </div>
+                }
+              })
+              .collect::<Vec<_>>()
+          }}
+        </div>
+      </section>
     </div>
   }
 }
